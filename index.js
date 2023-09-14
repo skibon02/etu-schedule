52a41867137ca7f7b456ca9d939702f60f337272
SkyGrel19 <skibav2002@gmail.com>
2023-09-15 00:11:52 +0300

    add client app

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
+import express from "express"
+import { Database } from "bun:sqlite";
 const readline = require("readline")
+const path = require('path')
+
+const db = new Database("db.sqlite", {create: true});
 
 let attend_token = 's%3A4sqTfnZzZ430ZvbcNAK6eesyiYZMTmfZ.5CsJTaCWfhNzFUzZvUHnZyuIYQst570B1OlymPMikZE';
+
+let api = express()
+
+// api.use(express.static(path.join(__dirname, "./client/build")))
+let port = 3000
+
+
+api.get('/api', (req, res) => {
+    res.send('hello world');
+})
+
+async function init_db() {
+
+}
 
 async function list_lessons() {
     let res = await fetch("https://digital.etu.ru/attendance/api/schedule/check-in", {
@@ -47,10 +66,10 @@ async function ask() {
     "\t3) exit\n", async (input_raw) => {
 
         let input = input_raw.split(" ")
-        console.log(input)
 
         switch (input[0]) {
-            case "1": 
+            case "1":
+
                 await list_lessons();
                 break;
             case "2":
@@ -58,16 +77,22 @@ async function ask() {
                 break;
 
             case "3": 
-                return
-                break;
+                process.exit()
+                return;
             
         }
-        ask()
+        await ask()
     });
 
 }
 
 async function app() {
+    await init_db();
+
+    api.listen(port, () => {
+      console.log(`Listening on port ${port}...`);
+    });
+
     await ask();
 }
 
