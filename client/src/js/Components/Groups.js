a7853a24c98d1adc5d50ec971e25457188874d60
Pyc-Nya <premiyaoskara@gmail.com>
2023-10-15 03:57:10 +0300

    fixed search

--- client/src/js/Components/Groups.js
+++ client/src/js/Components/Groups.js
@@ -6,34 +6,38 @@ export default function Groups({setGroup, setActive, groupList}) {
   
   return (
     <div className='groups'>
-      <div className='groups__input groups-input'>
-        <input 
-          className='groups-input__input'
-          type="text"
-          value={inputValue}
-          placeholder='       Номер группы'
-          onChange={(e) => setInputValue(e.target.value)}
-        />
-        {!inputValue && <img src={SEARCH} alt="" className='groups-input__icon'/>}
+      <div className='groups__input-container'>
+        <div className='groups__input groups-input'>
+          <input 
+            className='groups-input__input'
+            type="text"
+            value={inputValue}
+            placeholder='       Номер группы'
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          {!inputValue && <img src={SEARCH} alt="" className='groups-input__icon'/>}
+        </div>
       </div>
 
       {!groupList && <span className='groups__loading'>Загрузка...</span>}
 
-      {groupList && groupList
-      .filter((group) => 
-        group.number.indexOf(inputValue) === 0
-      )
-      .map((item) => (
-        <div 
-          className='groups__item'
-          key={item.id}
-          onClick={() => {
-            setGroup(item.id);
-            setActive('schedule');
-          }}>
-            {item.number}
-        </div>
-      ))}
+      <div className='groups__items'>
+        {groupList && groupList
+        .filter((group) => 
+          group.number.indexOf(inputValue) === 0
+        )
+        .map((item) => (
+          <div 
+            className='groups__item'
+            key={item.id}
+            onClick={() => {
+              setGroup(item.id);
+              setActive('schedule');
+            }}>
+              {item.number}
+          </div>
+        ))}
+      </div>
     </div>
   );
 
