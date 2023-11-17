5298ef8cd22bd1f103b1a29155ede46c9dfae012
SkyGrel19 <skibav2002@gmail.com>
2023-11-18 02:51:27 +0300

    introducing: attendance schedule diff

--- server/src/models/attendance.rs
+++ server/src/models/attendance.rs
@@ -1,6 +1,7 @@
 use std::collections::BTreeMap;
+use std::thread::yield_now;
 use sqlx::pool::PoolConnection;
-use sqlx::Sqlite;
+use sqlx::{Acquire, Sqlite};
 
 #[derive(sqlx::FromRow)]
 pub struct UserAttendanceScheduleModel {
@@ -24,8 +25,21 @@ pub async fn get_attendance_schedule(con: &mut PoolConnection<Sqlite>, user_id: 
     Ok(map)
 }
 
+
 pub async fn set_attendance_schedule(con: &mut PoolConnection<Sqlite>, user_id: u32,
                                      schedule_obj_time_link_id: u32, enable_auto_attendance: bool) -> anyhow::Result<()> {
+    let mut transaction = con.begin().await?;
+
+    //check old value
+    let old_value: Option<bool> = sqlx::query_scalar("SELECT enable_auto_attendance FROM user_attendance_schedule WHERE user_id = ? AND schedule_obj_time_link_id = ?")
+        .bind(user_id)
+        .bind(schedule_obj_time_link_id)
+        .fetch_optional(&mut transaction).await?;
+
+    if old_value.is_some() && old_value.unwrap() == enable_auto_attendance {
+        debug!("Skipping setting attendance schedule for user {} with schedule_obj_time_link_id {} and value {}", user_id, schedule_obj_time_link_id, enable_auto_attendance);
+        return Ok(());
+    }
 
     sqlx::query("INSERT INTO user_attendance_schedule (user_id, schedule_obj_time_link_id, enable_auto_attendance) VALUES (?, ?, ?) \
     ON CONFLICT(user_id, schedule_obj_time_link_id) DO UPDATE SET enable_auto_attendance = ?")
@@ -33,7 +47,75 @@ pub async fn set_attendance_schedule(con: &mut PoolConnection<Sqlite>, user_id: 
         .bind(schedule_obj_time_link_id)
         .bind(enable_auto_attendance)
         .bind(enable_auto_attendance)
-        .execute(&mut *con).await?;
+        .execute(&mut transaction).await?;
+
+    //invalidate diffs with old value
+    debug!("Invalidating diffs for user {} with schedule_obj_time_link_id {} and value {}", user_id, schedule_obj_time_link_id, enable_auto_attendance);
+    sqlx::query("DELETE FROM user_attendance_schedule_diffs WHERE user_id = ? AND schedule_obj_time_link_id = ? AND enable_auto_attendance = ?")
+        .bind(user_id)
+        .bind(schedule_obj_time_link_id)
+        .bind(enable_auto_attendance)
+        .execute(&mut transaction).await?;
+
+    transaction.commit().await?;
+
+    Ok(())
+}
+
+#[derive(sqlx::FromRow, Debug)]
+pub struct UserAttendanceScheduleDiffsModel {
+    pub user_id: u32,
+    pub schedule_obj_time_link_id: u32,
+    pub week_num: u32,
+    pub enable_auto_attendance: bool,
+}
+
+pub async fn get_attendance_schedule_diffs(con: &mut PoolConnection<Sqlite>, user_id: u32) -> anyhow::Result<BTreeMap<u32, Vec<(bool, u32)>>> {
+    let res = sqlx::query_as::<_, UserAttendanceScheduleDiffsModel>(
+        "SELECT * FROM user_attendance_schedule_diffs join schedule_objs \
+        on user_attendance_schedule_diffs.schedule_obj_time_link_id = schedule_objs.link_id WHERE user_id = ? AND gen_end IS NULL",
+    )
+        .bind(user_id)
+        .fetch_all(&mut *con).await?;
+
+    // link_id: (enable_auto_attendance, week_num)*
+    let mut map: BTreeMap<u32, Vec<(bool, u32)>> = BTreeMap::new();
+    for item in res {
+        map.entry(item.schedule_obj_time_link_id).or_default().push((item.enable_auto_attendance, item.week_num));
+    }
+
+    Ok(map)
+}
+
+pub async fn set_attendance_schedule_diff(con: &mut PoolConnection<Sqlite>, user_id: u32,
+                                           schedule_obj_time_link_id: u32, enable_auto_attendance: bool, week_num: u32) -> anyhow::Result<()> {
+    // get current value for attendance schedule
+    let current_value: Option<bool> = sqlx::query_scalar("SELECT enable_auto_attendance FROM user_attendance_schedule WHERE user_id = ? AND schedule_obj_time_link_id = ?")
+        .bind(user_id)
+        .bind(schedule_obj_time_link_id)
+        .fetch_optional(&mut *con).await?;
+
+    if current_value.is_some() && current_value.unwrap() == enable_auto_attendance {
+        //delete entry as redundant
+        debug!("Deleting entry for user {} with schedule_obj_time_link_id {} and week_num {} with value {}", user_id, schedule_obj_time_link_id, week_num, enable_auto_attendance);
+        sqlx::query("DELETE FROM user_attendance_schedule_diffs WHERE user_id = ? AND schedule_obj_time_link_id = ? AND week_num = ?")
+            .bind(user_id)
+            .bind(schedule_obj_time_link_id)
+            .bind(week_num)
+            .execute(&mut *con).await?;
+    }
+    else {
+        //insert entry
+        debug!("Inserting entry for user {} with schedule_obj_time_link_id {} and week_num {} with value {}", user_id, schedule_obj_time_link_id, week_num, enable_auto_attendance);
+        sqlx::query("INSERT INTO user_attendance_schedule_diffs (user_id, schedule_obj_time_link_id, enable_auto_attendance, week_num) VALUES (?, ?, ?, ?) \
+        ON CONFLICT(user_id, schedule_obj_time_link_id, week_num) DO UPDATE SET enable_auto_attendance = ?")
+            .bind(user_id)
+            .bind(schedule_obj_time_link_id)
+            .bind(enable_auto_attendance)
+            .bind(week_num)
+            .bind(enable_auto_attendance)
+            .execute(&mut *con).await?;
+    }
 
 
     Ok(())
--- server/src/routes/attendance.rs
+++ server/src/routes/attendance.rs
@@ -1,3 +1,4 @@
+use std::collections::BTreeMap;
 use rocket::Route;
 use rocket::serde::json::Json;
 use rocket_db_pools::Connection;
@@ -134,6 +135,141 @@ pub async fn set_user_attendance_schedule(mut db: Connection<Db>, auth: Option<A
     SetUserAttendanceScheduleResult::Success(Json(SetUserAttendanceScheduleResultSuccess { ok: true }))
 }
 
+
+
+#[derive(Serialize)]
+pub struct AttendanceScheduleDiffObj {
+    schedule_obj_time_link_id: u32,
+    is_new: bool,
+    auto_attendance_enabled: bool,
+}
+
+#[derive(Responder)]
+pub enum GetUserAttendanceScheduleDiffResult {
+    #[response(status = 200, content_type = "json")]
+    Success(Json<BTreeMap<u32, Vec<AttendanceScheduleObj>>>),
+    #[response(status = 400, content_type = "json")]
+    Failed(Json<ResponseErrorMessage>),
+}
+
+#[get("/attendance/schedule_diffs")]
+pub async fn get_user_attendance_schedule_diffs(mut db: Connection<Db>, auth: Option<AuthorizeInfo>) -> GetUserAttendanceScheduleDiffResult {
+    if auth.is_none() {
+        return GetUserAttendanceScheduleDiffResult::Failed(Json(ResponseErrorMessage::new("User is not authorized!".to_string())));
+    }
+    let auth = auth.unwrap();
+
+    // get user saved attendance schedule elements
+    let user_saved_elements = models::attendance::get_attendance_schedule_diffs(&mut db, auth.user_id).await;
+
+    if let Err(e) = user_saved_elements {
+        error!("Failed to get user attendance schedule: {:?}", e);
+        return GetUserAttendanceScheduleDiffResult::Failed(Json(ResponseErrorMessage::new("Failed to get user attendance schedule!".to_string())));
+    }
+    let user_saved_elements = user_saved_elements.unwrap();
+
+    let group_id = models::users::get_user_group(&mut db, auth.user_id).await;
+    if let Err(e) = group_id {
+        error!("Failed to get user group: {:?}", e);
+        return GetUserAttendanceScheduleDiffResult::Failed(Json(ResponseErrorMessage::new("Failed to get user group!".to_string())));
+    }
+    let group_id = group_id.unwrap();
+
+    if group_id.is_none() {
+        return GetUserAttendanceScheduleDiffResult::Failed(Json(ResponseErrorMessage::new("User has no group!".to_string())));
+    }
+    let group_id = group_id.unwrap().group_id;
+
+    // get user group link_id elements
+    let schedule_link_ids = models::schedule::get_current_schedule_link_ids(&mut db, group_id).await;
+    if let Err(e) = schedule_link_ids {
+        error!("Failed to get user group schedule link ids: {:?}", e);
+        return GetUserAttendanceScheduleDiffResult::Failed(Json(ResponseErrorMessage::new("Failed to get user group schedule link ids!".to_string())));
+    }
+    let schedule_link_ids = schedule_link_ids.unwrap();
+
+    let mut res = BTreeMap::new();
+    for link_id in schedule_link_ids {
+        let is_new = user_saved_elements.get(&link_id).is_none();
+        if let Some(link_id_elems) = user_saved_elements.get(&link_id).cloned() {
+            for link_id_elem in link_id_elems {
+                res.entry(link_id_elem.1).or_insert(Vec::new()).push(AttendanceScheduleObj {
+                    schedule_obj_time_link_id: link_id,
+                    is_new,
+                    auto_attendance_enabled: link_id_elem.0,
+                });
+            }
+        }
+
+    }
+
+    GetUserAttendanceScheduleDiffResult::Success(Json(res))
+}
+
+#[derive(Responder)]
+pub enum SetUserAttendanceDiffsScheduleResult {
+    #[response(status = 200, content_type = "json")]
+    Success(Json<SetUserAttendanceScheduleResultSuccess>),
+    #[response(status = 400, content_type = "json")]
+    Failed(Json<ResponseErrorMessage>),
+}
+
+#[derive(Deserialize)]
+pub struct SetUserAttendanceDiffsScheduleRequest {
+    schedule_obj_time_link_id: u32,
+    week_num: u32,
+    enable_auto_attendance: bool,
+}
+
+#[post("/attendance/schedule_diffs/update", data = "<data>")]
+pub async fn set_user_attendance_schedule_diffs(mut db: Connection<Db>, auth: Option<AuthorizeInfo>,
+                                          data: Json<SetUserAttendanceDiffsScheduleRequest>) -> SetUserAttendanceDiffsScheduleResult {
+    if auth.is_none() {
+        return SetUserAttendanceDiffsScheduleResult::Failed(Json(ResponseErrorMessage::new("User is not authorized!".to_string())));
+    }
+    let auth = auth.unwrap();
+
+    if data.week_num > 52 {
+        return SetUserAttendanceDiffsScheduleResult::Failed(Json(ResponseErrorMessage::new("Week number is too big!".to_string())));
+    }
+
+    // get user saved attendance schedule elements
+    let group_id = models::users::get_user_group(&mut db, auth.user_id).await;
+    if let Err(e) = group_id {
+        error!("Failed to get user group: {:?}", e);
+        return SetUserAttendanceDiffsScheduleResult::Failed(Json(ResponseErrorMessage::new("Failed to get user group!".to_string())));
+    }
+    let group_id = group_id.unwrap();
+
+    if group_id.is_none() {
+        return SetUserAttendanceDiffsScheduleResult::Failed(Json(ResponseErrorMessage::new("User has no group!".to_string())));
+    }
+    let group_id = group_id.unwrap().group_id;
+
+    // get user group link_id elements
+    let schedule_link_ids = models::schedule::get_current_schedule_link_ids(&mut db, group_id).await;
+
+    if let Err(e) = schedule_link_ids {
+        error!("Failed to get user group schedule link ids: {:?}", e);
+        return SetUserAttendanceDiffsScheduleResult::Failed(Json(ResponseErrorMessage::new("Failed to get user group schedule link ids!".to_string())));
+    }
+    let schedule_link_ids = schedule_link_ids.unwrap();
+
+    if !schedule_link_ids.contains(&data.schedule_obj_time_link_id) {
+        return SetUserAttendanceDiffsScheduleResult::Failed(Json(ResponseErrorMessage::new("Schedule object not found!".to_string())));
+    }
+
+    let res = models::attendance::set_attendance_schedule_diff(&mut db, auth.user_id, data.schedule_obj_time_link_id, data.enable_auto_attendance, data.week_num).await;
+
+    if let Err(e) = res {
+        error!("Failed to set user attendance schedule diff: {:?}", e);
+        return SetUserAttendanceDiffsScheduleResult::Failed(Json(ResponseErrorMessage::new("Failed to set user attendance schedule!".to_string())));
+    }
+
+    SetUserAttendanceDiffsScheduleResult::Success(Json(SetUserAttendanceScheduleResultSuccess { ok: true }))
+}
+
 pub fn get_routes() -> Vec<Route> {
-    routes![get_user_attendance_schedule, set_user_attendance_schedule]
+    routes![get_user_attendance_schedule, set_user_attendance_schedule,
+        get_user_attendance_schedule_diffs, set_user_attendance_schedule_diffs]
 }
