fe1216f90fb4fef9be894250237a1f1d6a839537
Pyc-Nya <premiyaoskara@gmail.com>
2023-12-05 00:03:44 +0300

    micro

--- client/src/js/JSX/Pages/Pages3.js
+++ client/src/js/JSX/Pages/Pages3.js
@@ -1,13 +1,6 @@
-import { useEffect, useState } from "react";
-import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux'
-import { useOnlineStatus } from "../../Utils/Hooks/useOnlineStatus";
-import { useDisableImageContextMenu } from "../../Utils/Hooks/useDisableImageContextMenu";
-import { routingFx } from "../../FxFetches/Pages/routingFx";
-import { setActiveByLocationFx } from "../../FxFetches/Pages/setActiveByLocationFx";
-import { groupScheduleFx } from "../../FxFetches/Pages/groupScheduleFx";
-import { vkDataFetch } from "../../ReduxStates/Slices/vkDataSlice";
-import { userDataGETFetch } from "../../ReduxStates/Slices/userDataSlice";
+import { Route, Routes } from 'react-router-dom';
+import { CSSTransition } from "react-transition-group";
+import { usePages } from "../../Utils/Hooks/usePages";
 import FISH from '../../../icons/fish.svg'
 import Header from "../Header/Header";
 import Schedule from '../Schedule/Schedule'
@@ -15,83 +8,38 @@ import Planning from "../Planning/Planning";
 import Profile from "../Profile/Profile";
 import NoMatchRoute from "../NoMatchRoute/NoMatchRoute";
 import NoSchedule from "../Schedule/NoSchedule";
-import { CSSTransition } from "react-transition-group";
-
-const TRANSITION_TIMEOUT = 300;
 
 export function Pages() {
-  const dispatch = useDispatch();
-  const location = useLocation();
-  const navigate = useNavigate();
-  const online = useOnlineStatus();
-  useDisableImageContextMenu();
+  const { active, vkData, fish } = usePages();
 
-  const { active } = useSelector(s => s.active);
-  const { groupId, groupNILoading } = useSelector(s => s.groupNI);
-  const { vkData } = useSelector(s => s.vkData);
-
-  const [fish, setFish] = useState(false);
-
-  useEffect(() => {
-    dispatch(vkDataFetch());
-    const handleFish = () => setFish(true);
-    window.addEventListener('fish', handleFish);
-    return () => {
-      window.removeEventListener('fish', handleFish);
-    };
-  }, [dispatch]);
-
-  useEffect(() => {
-    routingFx(navigate, location.pathname, vkData);
-    if (vkData && vkData.is_authorized && online) {
-      dispatch(userDataGETFetch(dispatch));
-    }
-  }, [dispatch, vkData, online]);
-
-  useEffect(() => {
-    if (online) {
-      groupScheduleFx(dispatch, groupId, groupNILoading);
-    }
-  }, [dispatch, groupId, online]);
-
-  useEffect(() => {
-    setActiveByLocationFx(dispatch, location)
-  }, [dispatch, location]);
-
-  if (!fish) {
-    if (!vkData) {
-      return <div className="schedule"><NoSchedule groupNumber={1} /></div>
-    } else if (!vkData.is_authorized) {
-      return (
-      <CSSTransition in={active === 'profile'} timeout={TRANSITION_TIMEOUT} classNames={'modal-transition'}>
-        <Profile />
-      </CSSTransition>
-      )
-    } else {
-        return (
-          <div className='container'>
-            <div className='under-header-box'></div>
-            {vkData.is_authorized && <Header />}
-            <Routes>
-              <Route path="*" element={<NoMatchRoute />} />
-              <Route path="/profile" element={
-                <CSSTransition in={active === 'profile'} timeout={TRANSITION_TIMEOUT} classNames={'modal-transition'} unmountOnExit>
-                  <Profile />
-                </CSSTransition>} />
-              <Route path="/schedule" element={
-                <CSSTransition in={active === 'schedule'} timeout={TRANSITION_TIMEOUT} classNames={'modal-transition'} unmountOnExit>
-                  <Schedule />
-                </CSSTransition>} />
-              <Route path="/planning" element={
-                <CSSTransition in={active === 'planning'} timeout={TRANSITION_TIMEOUT} classNames={'modal-transition'} unmountOnExit>
-                  <Planning />
-                </CSSTransition>} />
-            </Routes>
-            <div className='under-header-box-mobile'></div>
-          </div>
-        )
-      }
-  } else {
+  if (fish) {
     return <div className="fish"><img className="fish-image" src={FISH} alt="fish" draggable={false} /></div>
+  } else if (!vkData) {
+    return <div className="schedule"><NoSchedule groupNumber={1} /></div>
+  } else if (!vkData.is_authorized) {
+    return <Profile />
+  } else {
+    return (
+      <div className='container'>
+        <div className='under-header-box'></div>
+        {vkData.is_authorized && <Header />}
+        <Routes>
+          <Route path="*" element={<NoMatchRoute />} />
+          <Route path="/profile" element={
+            <CSSTransition in={active === 'profile'} timeout={300} classNames={'modal-transition'} unmountOnExit>
+              <Profile />
+            </CSSTransition>} />
+          <Route path="/schedule" element={
+            <CSSTransition in={active === 'schedule'} timeout={300} classNames={'modal-transition'} unmountOnExit>
+              <Schedule />
+            </CSSTransition>} />
+          <Route path="/planning" element={
+            <CSSTransition in={active === 'planning'} timeout={300} classNames={'modal-transition'} unmountOnExit>
+              <Planning />
+            </CSSTransition>} />
+        </Routes>
+        <div className='under-header-box-mobile'></div>
+      </div>
+    )
   }
 }
---
+++ client/src/js/Utils/Hooks/usePages.js
@@ -0,0 +1,52 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useOnlineStatus } from './useOnlineStatus';
+import { useDisableImageContextMenu } from './useDisableImageContextMenu';
+import { useState, useEffect } from 'react';
+import { vkDataFetch } from '../../ReduxStates/Slices/vkDataSlice';
+import { routingFx } from '../../FxFetches/Pages/routingFx';
+import { userDataGETFetch } from '../../ReduxStates/Slices/userDataSlice';
+import { groupScheduleFx } from '../../FxFetches/Pages/groupScheduleFx';
+import { setActiveByLocationFx } from '../../FxFetches/Pages/setActiveByLocationFx';
+
+export function usePages() {
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const online = useOnlineStatus();
+
+  useDisableImageContextMenu(); 
+
+  const { active } = useSelector(s => s.active);
+  const { groupId, groupNILoading } = useSelector(s => s.groupNI);
+  const { vkData } = useSelector(s => s.vkData);
+
+  const [fish, setFish] = useState(false);
+
+  useEffect(() => {
+    dispatch(vkDataFetch());
+    const handleFish = () => setFish(true);
+    window.addEventListener('fish', handleFish);
+    return () => window.removeEventListener('fish', handleFish);
+  }, [dispatch]);
+
+  useEffect(() => {
+    routingFx(navigate, location.pathname, vkData);
+    if (vkData && vkData.is_authorized && online) {
+      dispatch(userDataGETFetch(dispatch));
+    }
+  }, [dispatch, navigate, location, vkData, online]);
+
+  useEffect(() => {
+    if (online) {
+      groupScheduleFx(dispatch, groupId, groupNILoading);
+    }
+  }, [dispatch, groupId, groupNILoading, online]);
+
+  useEffect(() => {
+    setActiveByLocationFx(dispatch, location)
+  }, [dispatch, location]);
+
+  return { active, vkData, fish };
+}
+
